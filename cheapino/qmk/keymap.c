f40b3f462c4fc49f1cdc07186c7adc3e0b5e49d0
akamikado <manishvasireddy1234@gmail.com>
2025-11-13 10:08:57 +0530

    moved layout change key left side

--- cheapino/qmk/keymap.c
+++ cheapino/qmk/keymap.c
@@ -8,7 +8,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_Q,         KC_W,         KC_E,         KC_R,          KC_T,        KC_Y,  KC_U,         KC_I,         KC_O,         KC_P,
        LALT_T(KC_A), LGUI_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F),  KC_G,        KC_H,  RCTL_T(KC_J), RSFT_T(KC_K), RGUI_T(KC_L), RALT_T(KC_SCLN), 
        KC_Z,         KC_X,         KC_C,         KC_V,          KC_B,        KC_N,  KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,
-                                   TO(1),        KC_BSPC,       KC_NO,       TO(3), KC_SPC,       KC_ENT
+                                   TO(1),        KC_BSPC,       TO(3),       KC_NO, KC_SPC,       KC_ENT
        ),
 
     [1] = LAYOUT_split_3x5_3(
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_P,         KC_L,         KC_D,         KC_G,          KC_V,        KC_Q,  KC_F,         KC_O,         KC_U,         KC_COMM,
        LALT_T(KC_N), LGUI_T(KC_R), LSFT_T(KC_T), LCTL_T(KC_S),  KC_Y,        KC_J,  RCTL_T(KC_H), RSFT_T(KC_A), RGUI_T(KC_E), RALT_T(KC_I),
        KC_X,         KC_K,         KC_C,         KC_W,          KC_Z,        KC_B,  KC_M,         KC_QUOT,      KC_SCLN,       KC_DOT,
-                                   TO(4),        KC_BSPC,       KC_NO,       TO(0), KC_SPC,       KC_ENT
+                                   TO(4),        KC_BSPC,       TO(0),       KC_NO, KC_SPC,       KC_ENT
        ),
 
     [4] = LAYOUT_split_3x5_3(
